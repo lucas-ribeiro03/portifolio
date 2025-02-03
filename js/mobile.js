990baa480f92791676827df8e6b7edab7dcec493
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-02-03 17:27:12 -0300

    menu mobile

--- js/mobile.js
+++ js/mobile.js
@@ -1,11 +1,19 @@
 const mobileMenuButton = document.querySelector(".mobile-menu-button");
-
 mobileMenuButton.addEventListener("click", () => {
   const mobileMenu = document.querySelector(".mobile-menu");
   const mobileItem = document.querySelector(".mobile-ul");
+
   if (!mobileMenu.classList.contains("open")) {
-    mobileMenu.classList.add("open");
     mobileMenu.style.display = "block";
     mobileItem.style.display = "flex";
+    mobileMenu.classList.add("open");
+    document.body.style.overflow = "hidden";
+    mobileMenuButton.classList.replace("fa-bars", "fa-x");
+  } else if (mobileMenu.classList.contains("open")) {
+    mobileMenu.style.display = "none";
+    mobileItem.style.display = "none";
+    mobileMenu.classList.remove("open");
+    mobileMenuButton.classList.replace("fa-x", "fa-bars");
+    document.body.style.overflow = "auto";
   }
 });
