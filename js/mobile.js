3f52e9c61e73da1c30a8f54d2a6683376c00527b
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-02-03 16:48:28 -0300

    menu

--- js/mobile.js
+++ js/mobile.js
@@ -2,4 +2,8 @@ const mobileMenuButton = document.querySelector(".mobile-menu-button");
 
 mobileMenuButton.addEventListener("click", () => {
   const mobileMenu = document.querySelector(".mobile-menu");
+  if (!mobileMenu.classList.contains("open")) {
+    mobileMenu.classList.add("open");
+    mobileMenu.style.display = "block";
+  }
 });
