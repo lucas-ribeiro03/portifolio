6bf3133e769f5f2a1008b3a74c042227dd678d44
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-02-03 16:22:12 -0300

    responsivity

---
+++ js/mobile.js
@@ -0,0 +1,5 @@
+const mobileMenuButton = document.querySelector(".mobile-menu-button");
+
+mobileMenuButton.addEventListener("click", () => {
+  const mobileMenu = document.querySelector(".mobile-menu");
+});
