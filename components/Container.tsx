75c15e4cade35d4607d6f2cafb7f89015adbaf3a
lucas-ribeiro03 <lucasribeirollr05@gmail.com>
2026-03-16 08:51:26 -0300

    fix: width set by percentage

--- components/Container.tsx
+++ components/Container.tsx
@@ -9,7 +9,7 @@ const Container = ({ children, id }: ContainerProps) => {
   return (
     <section
       id={id}
-      className="py-16 px-4 lg:px-0 flex flex-col gap-8 items-center min-h-screen justify-center lg:w-395 lg:ml-auto *:z-10 overflow-x-hidden"
+      className="py-16 px-4 lg:px-0 flex flex-col gap-8 items-center min-h-screen justify-center lg:w-[75%] lg:ml-auto *:z-10 overflow-x-hidden "
     >
       {children}
     </section>
